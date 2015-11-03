66dc83812a9de4d131ef5eac91af85f1e4237e98
Jeff Lee <jlee116057@gmail.com>
2015-11-02 16:35:57 -0800

    app.ts database update
    app.ts route to db has been updated from nodetest1 to sprint1db

--- public/javascripts/ServerCommService.ts
+++ public/javascripts/ServerCommService.ts
@@ -1,13 +1,22 @@
+///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
+///<reference path='../types/DefinitelyTyped/express/express.d.ts'/> 
 class ServiceCommService {
-/* To add here: Facebook login and FAA request */
+    constructor(){
+        var express = require('express');
+        var mongo = require('mongodb');
+        var monk = require('monk');
+        var db = monk('localhost:27017/sprint1db');
+        /* To add here: Facebook login and FAA request */
 
-// FaceBook Login Check
-  function checkIfInDatabase(var id) {
+        // FaceBook Login Check
+          function checkIfInDatabase(var id) {
 
-  }
-  
-  function addToDatabase(var id) {
+          }
 
-  }
+          function addToDatabase(var id) {
+
+          }
+
+        }
 
 }
