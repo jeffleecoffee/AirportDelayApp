f6af54969cf895c3eb63e2b989413221d711701d
Jeff Lee <jlee116057@gmail.com>
2015-11-03 16:56:26 -0800

    Currently working on a temp FBLoginManager which will be added to servercomm

--- public/javascripts/FBLoginManager.js
+++ public/javascripts/FBLoginManager.js
@@ -1,1 +1,48 @@
-alert("hello");
+///<reference path='../../types/DefinitelyTyped/node/node.d.ts'/>
+///<reference path='../../types/DefinitelyTyped/express/express.d.ts'/> 
+var FBLoginManager = (function () {
+    function FBLoginManager() {
+        var express = require('express');
+        var path = require('path');
+        var favicon = require('serve-favicon');
+        var logger = require('morgan');
+        var cookieParser = require('cookie-parser');
+        var bodyParser = require('body-parser');
+        var mongo = require('mongodb');
+        var monk = require('monk');
+        var db = monk('localhost:27017/sprint1db');
+        var fbloginmanager = express.FBLoginManager();
+        // FaceBook Login Check
+        function checkIfInDatabase(id) {
+            return true;
+        }
+        function addToDatabase(id) {
+            return;
+        }
+        // //POST to Add User Service 
+        // router.post('/adduser', function(req, res) {
+        // // Set our internal DB variable
+        // var db = req.db;
+        // // Get our form values. These rely on the "name" attributes
+        // var user = new User(req.body.username,req.body.useremail);
+        // // Set our collection
+        // var collection = db.get('usercollection');
+        // // Submit to the DB
+        // collection.insert({
+        //     "username" : user.getName(),
+        //     "email" : user.getEmail(),
+        // }, function (err, doc) {
+        //     if (err) {
+        //         // If it failed, return error
+        //         res.send("There was a problem adding the information to the database.");
+        //     }
+        //     else {
+        //         // And forward to success page
+        //         res.redirect("userlist");
+        //     }
+        // });
+        module.exports = fbloginmanager;
+    }
+    return FBLoginManager;
+})();
+var fbloginmanager = new FBLoginManager();
--- public/javascripts/FBLoginManager.ts
+++ public/javascripts/FBLoginManager.ts
@@ -1,24 +1,60 @@
 ///<reference path='../../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='../../types/DefinitelyTyped/express/express.d.ts'/> 
 
-///<reference path='./Airport.ts'/> 
-
 class FBLoginManager {
 
-        /* To add here: Facebook login and FAA request */
-    constructor(){
-        var express = require('express');
-        var mongo = require('mongodb');
-        var monk = require('monk');
-        var db = monk('localhost:27017/sprint1db');
-
-            // FaceBook Login Check
-        function checkIfInDatabase(var id) {
-
-        }
-        function addToDatabase(var id) {
+  constructor(){
+    var express = require('express');
+    var path = require('path');
+    var favicon = require('serve-favicon');
+    var logger = require('morgan');
+    var cookieParser = require('cookie-parser');
+    var bodyParser = require('body-parser');
+    var mongo = require('mongodb');
+    var monk = require('monk');
+    var db = monk('localhost:27017/sprint1db');
 
 
-            }
+    var fbloginmanager = express.FBLoginManager();
+
+        // FaceBook Login Check
+    function checkIfInDatabase(id: number) {
+      return true;
     }
+    function addToDatabase(id: number) {
+      return;
+    }
+
+    // //POST to Add User Service 
+    // router.post('/adduser', function(req, res) {
+
+    // // Set our internal DB variable
+    // var db = req.db;
+
+    // // Get our form values. These rely on the "name" attributes
+    // var user = new User(req.body.username,req.body.useremail);
+
+    // // Set our collection
+    // var collection = db.get('usercollection');
+
+    // // Submit to the DB
+    // collection.insert({
+    //     "username" : user.getName(),
+    //     "email" : user.getEmail(),
+    // }, function (err, doc) {
+    //     if (err) {
+    //         // If it failed, return error
+    //         res.send("There was a problem adding the information to the database.");
+    //     }
+    //     else {
+    //         // And forward to success page
+    //         res.redirect("userlist");
+    //     }
+    // });
+
+
+
+    module.exports = fbloginmanager;
+  }
 }
+var fbloginmanager = new FBLoginManager();
