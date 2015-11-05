8e2bb7ee5c2aac6d89f1882fb933f45efbf4ba58
Solon Zhu <solon.zhu@motitopia.com>
2015-11-04 22:14:01 -0800

    ResultsView has hardcoded values, matches MapView

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 ///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='../types/DefinitelyTyped/express/express.d.ts'/> 
+///<reference path='../public/javascripts/ServerCommService.ts'/> 
 var ViewRouter = (function () {
     function ViewRouter() {
     }
     ViewRouter.createRouter = function () {
         var express = require('express');
         var router = express.Router();
+        var monk = require('monk');
         /* GET home page. */
         router.get('/', function (req, res, next) {
             res.render('LoginView', { title: 'AirTime' });
@@ -14,21 +16,25 @@ var ViewRouter = (function () {
             res.render('RequestView', { title: 'AirTime' });
         });
         router.get('/ResultView', function (req, res) {
-            res.render('ResultView', { title: 'AirTime' });
+            var results = [{ "code": "BHM", "name": "Birmingham-Shuttlesworth International Airport", "temp": "12 Degrees Celcius", "wind": "50 km/h" },
+                { "code": "ANC", "name": "Ted Stevens Anchorage International Airport", "temp": "5 Degrees Celcius", "wind": "50 km/h" },
+                { "code": "ILG", "name": "New Castle Airport", "temp": "10 Degrees Celcius", "wind": "60 km/h" },
+                { "code": "MIA", "name": "Miami International Airport", "temp": "25 Degrees Celcius", "wind": "40 km/h" }];
+            res.render('ResultView', { title: 'AirTime', resultsList: results });
         });
         router.get('/MapView', function (req, res) {
             res.render('MapView', { title: 'AirTime' });
         });
-        /* GET Userlist page. */
-        router.get('/airportlist', function (req, res) {
+        /* GET Userlist page.
+        router.get('/airportlist', function(req, res) {
             var db = req.db;
             var collection = db.get('airports');
-            collection.find({}, {}, function (e, docs) {
-                res.render('airportlist', {
-                    "airportlist": docs
-                });
-            });
-        });
+            collection.find({},{},function(e,docs){
+                    res.render('airportlist', {
+                            "airportlist" : docs
+                    });
+               });
+        });*/
         return router;
     };
     return ViewRouter;
--- routes/index.ts
+++ routes/index.ts
@@ -1,11 +1,12 @@
 ///<reference path='../types/DefinitelyTyped/node/node.d.ts'/>
 ///<reference path='../types/DefinitelyTyped/express/express.d.ts'/> 
+///<reference path='../public/javascripts/ServerCommService.ts'/> 
 class ViewRouter {
 
     static createRouter() {
         var express = require('express');
         var router = express.Router();
-
+		var monk = require('monk');
         /* GET home page. */
 
         router.get('/', function(req, res, next) {
@@ -15,12 +16,16 @@ class ViewRouter {
           res.render('RequestView', {title: 'AirTime' });
         });
         router.get('/ResultView', function(req, res) {
-          res.render('ResultView', {title: 'AirTime' });
+		  var results = [{"code": "BHM", "name": "Birmingham-Shuttlesworth International Airport", "temp": "12 Degrees Celcius", "wind": "50 km/h"},
+						{"code": "ANC", "name": "Ted Stevens Anchorage International Airport", "temp": "5 Degrees Celcius", "wind": "50 km/h"},
+						{"code": "ILG", "name": "New Castle Airport", "temp": "10 Degrees Celcius", "wind": "60 km/h"},
+						{"code": "MIA", "name": "Miami International Airport", "temp": "25 Degrees Celcius", "wind": "40 km/h"}]
+          res.render('ResultView', {title: 'AirTime', resultsList: results});
         });
         router.get('/MapView', function(req, res) {
           res.render('MapView', {title: 'AirTime' });
         });
-        /* GET Userlist page. */
+        /* GET Userlist page. 
         router.get('/airportlist', function(req, res) {
             var db = req.db;
             var collection = db.get('airports');
@@ -29,7 +34,7 @@ class ViewRouter {
         	                "airportlist" : docs
         		    });
                });
-        });
+        });*/
         return router;
     }
 }
