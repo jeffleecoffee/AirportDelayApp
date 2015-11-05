39d1aa496c9f9cc4a63f9e53b310fd6ddd23ed19
Jeff Lee <jlee116057@gmail.com>
2015-11-04 21:55:20 -0800

    more heroku config. using process.env to configure stuff

--- routes/auth.js
+++ routes/auth.js
@@ -3,30 +3,43 @@
 var AuthRouter = (function () {
     function AuthRouter() {
         var express = require('express');
-        var router = express.Router();
         var passport = require('passport');
         var FacebookStrategy = require('passport-facebook').Strategy;
         var mongo = require('mongodb');
         var monk = require('monk');
-        var db = monk('localhost:27017/sprint1db');
+        var facebookCallbackUrl;
+        var db;
+        if (process.env.NODE_ENV) {
+            db = monk(process.env.MONGO_URL, {
+                username: process.env.MONGO_USER,
+                password: process.env.MONGO_PASSWORD
+            });
+            facebookCallbackUrl = process.env.URL + "/auth/facebook/callback";
+        }
+        else {
+            db = monk('localhost:27017/sprint1db');
+            facebookCallbackUrl = "http://localhost:3000/auth/facebook/callback";
+        }
+        var router = express.Router();
         var users = db.get('users');
         passport.serializeUser(function (user, done) {
             done(null, user.uid);
         });
         passport.deserializeUser(function (id, done) {
             users.findOne({ uid: id }, function (err, doc) {
-                done(err, { uid: doc.uid });
+                done(err, { uid: doc.uid }); //make user object
             });
         });
         passport.use(new FacebookStrategy({
             clientID: '1646694312239569',
             clientSecret: '01458760d8b21ff2649a8d029b480f57',
-            callbackURL: "http://localhost:3000/auth/facebook/callback",
+            callbackURL: facebookCallbackUrl,
             enableProof: false
         }, function (accessToken, refreshToken, profile, done) {
-            users.update({ uid: profile.id }, { uid: profile.id }, { upsert: true }, function (err, numberOfDocumentsUpdated, documents) {
+            users.update({ uid: profile.id }, { uid: profile.id }, { upsert: true }, //these should be in a user service
+            function (err, numberOfDocumentsUpdated, documents) {
                 users.findOne({ uid: profile.id }, function (err, doc) {
-                    done(err, { uid: doc.uid });
+                    done(err, { uid: doc.uid }); //make user object
                 });
             });
         }));
